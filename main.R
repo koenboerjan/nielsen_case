4cd9b20982c37a65ed5e145cbfedadfabc7e0860
Koen Boerjan <boerjankoen@gmail.com>
2025-01-28 11:09:04 +0100

    Added requiremnents file, and set up insights file

--- main.R
+++ main.R
@@ -3,20 +3,16 @@ rm(list = ls())
 
 #Load libraries and functions
 prepare_setup <- function() {
-  library(dplyr)
-  library(tidyr)
-  library(purrr)
-  library(readr)
-  source("likelihood_functions.R")
-  source("simulate_data.R")
-  source("read_data.R")
+  source("requirements.R")
+  install_requirements()
 }
 
 
 prepare_setup()
 
 n <- 1000000
-dataset <- simulate_dataset_male_female(n, 0.1, 0.1, 0.6, 0.7)
+dataset <- simulate_dataset_male_female(n, fraction_male = 0.7, exposure_male = 0.03, exposure_female = 0.05,
+                                        estimate_correct_male = 0.6, estimate_correct_female = 0.7)
 
 real_dataset <- read_exposures()
 head(real_dataset)
