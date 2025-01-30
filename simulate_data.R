e2fbdb1fdb6f7fa375075cfffce82973a053fdb7
Koen Boerjan <703167kb@eur.nl>
2025-01-30 16:21:21 +0100

    reformated data_simulation to be able to simulate for all segmentation

--- simulate_data.R
+++ simulate_data.R
@@ -1,27 +1,48 @@
-simulate_dataset_male_female <- function(n_test, fraction_male, exposure_male, exposure_female, estimate_correct_male, 
-                                         estimate_correct_female,  seed_number = 0) {
+simulate_dataset <- function(n_test, fraction_per_segment, exposure_per_segment, estimation_correctness,
+                             segmentation, seed_number = 0) {
+
   set.seed(seed_number)
-  male_response <- rbinom(n_test*fraction_male, 1, exposure_male)
-  female_response <- rbinom(n_test*(1-fraction_male), 1, exposure_female)
-  responses <- c(male_response, female_response)
+  segments_count <- length(fraction_per_segment)
   
-  male_estimates <- rbinom(n_test*fraction_male, 1, estimate_correct_male)
-  female_estimates <- rbinom(n_test*(1-fraction_male), 1, (1 - estimate_correct_female))
-  estimates <- c(male_estimates, female_estimates)
+  if (sum(fraction_per_segment) != 1 || sum(estimation_correctness) != segments_count) {
+    print(sum(estimation_correctness))
+    stop("Fractions of segments do not add up to 1")
+  }
   
-  true_gender <- c(rep(1,n_test*fraction_male), rep(0,n_test*(1-fraction_male)))
   
-  sample_df <- data.frame(estimates, true_gender, responses)
-  colnames(sample_df) <- c("estimated_gender",  "true_gender", "response")
+  segments <- seq(segments_count)
+  responses <- c()
+  estimate_segments <- c()
+  true_segments <- c()
+  true_sampled_reach <- c()
   
-  true_male_reach <- sum(male_response)/(n_test*fraction_male)
-  true_female_reach <- sum(female_response)/(n_test*(1-fraction_male))
+  for (i in 1:segments_count) {
+    response_segment <- rbinom(n_test*fraction_per_segment[i], 1, exposure_per_segment[i])
+    responses <- c(responses, response_segment)
+    estimate_segments <- c(estimate_segments, sample(seq(segments), n_test*fraction_per_segment[i], 
+                                                     replace = TRUE, prob = estimation_correctness[i,]))
+    true_segments <- c(true_segments, rep(i,n_test*fraction_per_segment[i]))
+    true_sampled_reach <- c(true_sampled_reach, sum(response_segment)/(n_test*fraction_per_segment[i]))
+    print(true_sampled_reach)
+  }
   
-  cat(paste('Dataset for male and female devision has been generated, input parameters are: \n sample size: ', n_test, 
-              '\n exposure male:', exposure_male, '\n exposure female: ', exposure_female, 
-              '\n correct estimation male: ', estimate_correct_male, '\n correct estimation female: ', 
-              estimate_correct_female, '\nTrue sampled reach are, \n for male: ', true_male_reach, '\n for female: ',
-              true_female_reach))
+  sample_df <- data.frame(estimate_segments, true_segments, responses)
+  if (segmentation == 'gender') {
+    colnames(sample_df) <- c("estimated_gender", "true_gender", "response")
+  } else if (segmentation == 'age') {
+    colnames(sample_df) <- c("estimated_age", "true_age", "response")
+  } else if (segmentation == 'demo') {
+    colnames(sample_df) <- c("estimated_demo", "true_demo", "response")
+  } else {
+    close('Segmentation does not exist')
+  }
+  
+  cat(paste('Dataset for male and female devision has been generated, input parameters are: \n sample size: ', n_test))
+  for (i in 1:segments_count) {
+    cat('\nSegment: ', i,'\n fractions of segment:', fraction_per_segment[i], '\n exposure of segment:', exposure_per_segment[i], 
+              '\n correct estimation segments: ', paste(estimation_correctness[i,]), '\n true sampled reach is: ', true_sampled_reach[i])
+  }
+
   
   return(sample_df)
 }
