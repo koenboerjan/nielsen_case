e61b1f59e6260e4dd23ffd2ca90997eff4f4a4ee
Koen Boerjan <boerjankoen@gmail.com>
2025-02-03 15:10:02 +0100

    small update

--- simulation_study.R
+++ simulation_study.R
@@ -217,7 +217,7 @@ simulate_evaluate_exposure_2_segments <- function() {
   x_plane <- exposure_1[c(1,1,12,12)]
   y_plane <- exposure_2[c(1, 12, 1, 12)]
   z_plane_1 <- exposure_1[c(1,1,12,12)]
-  z_plane_2 <- exposure_2[c(1,1,12,12)]
+  z_plane_2 <- exposure_2[c(1,12,1,12)]
   
   # Create plot1
   plot1 <- plot_ly() %>%
@@ -265,8 +265,113 @@ simulate_evaluate_exposure_2_segments <- function() {
            scene = list(xaxis = list(title = "Exposure 1"),
                         yaxis = list(title = "Exposure 2"),
                         zaxis = list(title = "Reach segment 2", range = c(0, 1))))
-  print(plot1)
+  print(plot2)
   
 }
+
+
+### Simulation estimation accuracy exposure
+simulate_evaluate_accuracy_2_segments <- function() {
+  fraction_gender <- c(0.516, 0.484)
+  exposure <- c(0.03, 0.05)
+  accuracy_estimate_1 <- c(seq(0.5, 0.65, 0.025), seq(0.7, 0.95, 0.05))
+  accuracy_estimate_2 <- c(seq(0.5, 0.65, 0.025), seq(0.7, 0.95, 0.05))
+  accuracy_estimation <- data.frame(segment_1_correct = numeric(), segment_1_wrong = numeric(), 
+                                    segment_2_correct = numeric(),segment_2_wrong = numeric())
+  
+  for (i in 1:length(accuracy_estimate_1)) {
+    for (j in 1:length(accuracy_estimate_2)) {
+      accuracy_estimation <- rbind(accuracy_estimation, c(accuracy_estimate_1[i], 1 - accuracy_estimate_1[i], 
+                                                          1- accuracy_estimate_2[j], accuracy_estimate_2[j]))
+    }
+  }
+  accuracy_estimation <- as.matrix(accuracy_estimation)
+   
+  n_test <- 10**6
+  
+  final_results <- data.frame(segment1 = numeric(), segment2 = numeric())
+  for (r in 1:10) {
+    print(r)
+    results <- data.frame(segment1 = numeric(), segment2 = numeric())
+    for (i in 1:dim(accuracy_estimation)[1]) {
+      print(i)
+      estimation_correctness_mat <- as.matrix(t(matrix(accuracy_estimation[i,], ncol = 2)))
+      dataset <- simulate_dataset(n_test = n_test, fraction_per_segment = fraction_gender, 
+                                  exposure_per_segment = exposure, estimation_correctness = estimation_correctness_mat,
+                                  segmentation = "gender", seed_number = (i*10 + 15*r), print_simulation = FALSE)
+      optimal_beta <- optimize_loglikelihood(dataset, segmentation = 'gender', with_prior = FALSE, print_result = FALSE)
+      results <- rbind(results, exp(optimal_beta$par) / (1 + exp(optimal_beta$par)))
+      
+    }
+    if (r == 1) {
+      final_results <- results
+    } else {
+      final_results <- final_results + results
+    }
+  }
+  
+  final_results <- final_results/10
+  final_results$correctness_1 <- accuracy_estimation[,1]
+  final_results$correctness_2 <- accuracy_estimation[,4]
+  
+  colnames(final_results) <- c('segment_1', 'segment_2', 'segment_1_accuracy', 'segment_2_accuracy')
+  
+  x_plane <- c(0.5, 0.5, 1, 1)
+  y_plane <- c(0.5, 1, 0.5, 1)
+  z_plane_1 <- rep(0.03, 4)
+  z_plane_2 <- rep(0.05, 4)
+  
+  # Create plot1
+  plot1 <- plot_ly() %>%
+    # Add 3D Scatter Points
+    add_trace(
+      data = final_results,
+      x = ~segment_1_accuracy, y = ~segment_2_accuracy, z = ~segment_1,
+      type = "scatter3d",
+      mode = "markers",
+      marker = list(size = 5),
+      name = "Segment 1"
+    ) %>%
+    # Add a Transparent Plane
+    add_trace(
+      x = x_plane, y = y_plane, z = z_plane_1,
+      type = "mesh3d",
+      opacity = 0.5,
+      color = "red",
+      showscale = FALSE
+    )  %>%
+    layout(title = "Reach of segment 1 given segment exposures",
+           scene = list(xaxis = list(title = "Exposure 1"),
+                        yaxis = list(title = "Exposure 2"),
+                        zaxis = list(title = "Reach segment 1", range = c(0, 0.1))))
+  print(plot1)
+  
+  # Create plot2
+  plot2 <- plot_ly() %>%
+    # Add 3D Scatter Points
+    add_trace(
+      data = final_results,
+      x = ~segment_1_accuracy, y = ~segment_2_accuracy, z = ~segment_2,
+      type = "scatter3d",
+      mode = "markers",
+      marker = list(size = 5),
+      name = "Segment 2"
+    ) %>%
+    # Add a Transparent Plane
+    add_trace(
+      x = x_plane, y = y_plane, z = z_plane_2,
+      type = "mesh3d",
+      opacity = 0.5,
+      color = "red",
+      showscale = FALSE
+    ) %>%
+    layout(title = "Reach of segment 2 given segment exposures",
+           scene = list(xaxis = list(title = "Exposure 1"),
+                        yaxis = list(title = "Exposure 2"),
+                        zaxis = list(title = "Reach segment 2", range = c(0, 0.1))))
+  print(plot2)
+  
+}
+
 
 
