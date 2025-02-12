5a8e7d43c6da8016fe93d2045dba28dc88e784d8
Pauline Cox <523644pc@eur.nl>
2025-02-12 11:38:06 +0100

    quicker data reading

--- requirements.R
+++ requirements.R
@@ -1,3 +1,7 @@
+# Install required packages
+# -----------------------
+
+
 install_requirements <- function() {
   library(dplyr)
   library(tidyr)
@@ -6,6 +10,9 @@ install_requirements <- function() {
   library(ggplot2)
   library(plotly)
   library(data.table)
+  library(future.apply)
+  library(duckdb)
+  library(DBI)
   source("likelihood_functions.R")
   source("simulate_data.R")
   source("read_data.R")
